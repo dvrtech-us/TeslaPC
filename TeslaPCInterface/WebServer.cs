d9c002269662b578a6d30104cda34f6724ebeffa
DVR <david@vanronk.net>
2024-07-28 13:54:42 -0500

    Update WebServer.cs

--- TeslaPCInterface/WebServer.cs
+++ TeslaPCInterface/WebServer.cs
@@ -159,7 +159,7 @@ public class WebServer
                 string? path = QueryParameters.Get("path");
                 if (path == null)
                 {
-                    guts = returnAllFilesAsHtmlLinks("E:\\");
+                    guts = returnAllFilesAsHtmlLinks("C:\\video\\");
                 }
                 else
                 {
